1221dbff253e43e949b9ad7e2a1e2cc66aae60af
harshiling <harshiling@gmail.com>
2020-01-04 18:27:58 +0530

    Commit

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int a;
-    cin>>a;
-    cout<<a/2<<end;
+    int Weight;
+    cin >> Weight;
+
+    if (Weight % 2 == 0 && Weight != 2)
+    {
+        cout << "YES \n";
     }
+
+    else
+    {
+        cout << "NO \n";
+    }
+
+    return 0;
+}
