3d5cbc147487601486f66ee56dfa16b18adde4cc
harshiling <harshiling@gmail.com>
2020-01-22 18:29:17 +0530

    Update due to OS Change

--- OBubblesort.cpp
+++ OBubblesort.cpp
@@ -4,36 +4,38 @@ using namespace std;
 
 int main()
 {
-    int length  = 9 , count;
-    int array[length] = { 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+    int length  ;
+    cin>>length;
+    int array[length] ;
 
-    for(int s_i = 0 ; s_i<length-1 ; s_i++) //s_i is the starting index which is 0 initially
+    for(int i = 0 ; i<length ; i++)
+    cin>>array[i];
+
+
+      for(int s_i = 0 ; s_i<length-1 ; s_i++) //s_i is the starting index which is 0 initially
     { 
-        count = 0;
        
-        for(int c_i = 0 ; c_i <length; c_i++) //c_i is the next index to start index
+        for(int c_i = 0 ; c_i <length-1; c_i++) //c_i is the next index to start index
         {
-          if(array[c_i] > array[c_i+1])
+          if(array[c_i] < array[c_i+1])
           {
           swap(array[c_i] , array[c_i+1]);
-          count = 1;
+          
           
           }
         }
        cout<<endl;
-       if(count == 0){
-       cout<<"outer loop terminated on"<<" "<<s_i+1<<"th iteration"<<endl;
-       break;
-      }
-     
+       
+    }
+      for(int i = 0 ; i<length ; i++)
+    cout<<array[i]<<" "<<endl;
      
 
     }
 
-    cout<<"Sorted array"<<endl;
-    for(int i = 0 ; i<length ; i++)
-    cout<<array[i]<<" "<<endl;
-}
+   
+   
+
      
        
         
